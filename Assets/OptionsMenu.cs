d43a4ad8ddca4e2492536464730a97e58383a7ec
Vedran Nejedly <62423202+VedranNejedly@users.noreply.github.com>
2023-05-05 01:03:35 +0200

    Options menu update
    Updated options menu so resolution can be changed, fullscreen can be toggled and quality settings can be selected.

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class OptionsMenu : MonoBehaviour
+{   
+    public TMP_Dropdown resolutionsDropdown;
+    Resolution[] resolutionsArray;
+    int pcResolutionIndex = 0;
+
+    void Start(){
+        resolutionsArray = Screen.resolutions;
+        resolutionsDropdown.ClearOptions();
+        List<string> options = new List<string>();
+
+        for(int i=0;i<resolutionsArray.Length;i++){
+            string option = resolutionsArray[i].width+ " x " +resolutionsArray[i].height; 
+            options.Add(option);
+
+            if (resolutionsArray[i].width == Screen.currentResolution.width && resolutionsArray[i].height == Screen.currentResolution.height){
+                pcResolutionIndex=i;
+            }
+        }
+
+        resolutionsDropdown.AddOptions(options);
+        resolutionsDropdown.value = pcResolutionIndex;
+        resolutionsDropdown.RefreshShownValue();
+
+    }
+
+    public void updateResolution(int resolutionIndex){
+        Resolution resolution = resolutionsArray[resolutionIndex];
+        Screen.SetResolution(resolution.width,resolution.height, Screen.fullScreen);
+    }
+
+    public void setGraphicQuality(int qualityIndex){
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void fullscreenToggle(bool isFullscreen){
+        Screen.fullScreen = isFullscreen;
+    }
+}
+ 
