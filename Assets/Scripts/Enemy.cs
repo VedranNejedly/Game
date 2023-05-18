f1d0ef7a14623c697f349e385e8491755aaace7b
Vedran Nejedly <62423202+VedranNejedly@users.noreply.github.com>
2023-05-18 10:43:15 +0200

    Monter attack and nav chase player.
    Monster chase player as Nav Agent and monster attack player with cooldown on attacks.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,19 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-
     EnemySpawnAndKillCount esakc;
     TriggerNextLevel tnl;
+    PlayerHealth PlayerHealth;
+
     public Animator animator;
     public int health = 10;
     public bool isBoss = false;
     public bool bossIsDead = false;
+    public int damage = 5;
+
+    public NavMeshAgent nav;
+    public Transform player;
+
+
+    public float attackCooldown = 3.0f;
+    public bool enemyAlreadyAttacked = false;
+
+    public float sightRange,attackRange;
+    public bool playerInSight, playerInAttackRange;
 
     void Start(){
         esakc = GameObject.FindGameObjectWithTag("Player").GetComponent<EnemySpawnAndKillCount>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        PlayerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update(){
+        // //If player is in sight and in attack range
+        // playerInAttackRange=Physics.CheckSphere(transform.position,attackRange,whatIsPlayer);
+        // playerInSight=Physics.CheckSphere(transform.position,sightRange,whatIsPlayer);
+    
+        // if(playerInSight && !playerInAttackRange){
+            ChasePlayer();
+        // }
+        // if(playerInAttackRange && playerInSight){
+        //     AttackPlayer();
+        // }
+    
     }
 
     public void Damage(int damage){
@@ -41,18 +72,49 @@ public class Enemy : MonoBehaviour
 
 
 
+    private void ChasePlayer(){
+        nav.SetDestination(player.position);
+        Debug.Log("I am chasing");
+    }
+
+    private void AttackPlayer(){
+        nav.SetDestination(transform.position);
+        transform.LookAt(player);
+        if(enemyAlreadyAttacked){
+            return;
+        }
+        animator.SetBool("isAttacking",true);
+        PlayerHealth.updateHealth(-damage);
+        enemyAlreadyAttacked = true;
+        Invoke("ResetAttack",3.0f);
+        // StartCoroutine(ResetAttack());
+    }
 
 
-    // private void OnTriggerEnter(Collider other){
-    //     if(other.gameObject.tag=="Player"){
-    //         animator.SetBool("isAttacking",true);
-    //     }
-    // }
-    //  private void OnTriggerExit(Collider other){
-    //     if(other.gameObject.tag=="Player"){
-    //         animator.SetBool("isAttacking",false);
-    //     }
-    // }
+    private void ResetAttack(){
+        enemyAlreadyAttacked = false;
+        animator.SetBool("isAttacking",false);
+
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag=="Player"){
+            AttackPlayer();
+        }
+    }
+
+     private void OnTriggerExit(Collider other){
+        if(other.gameObject.tag=="Player"){
+            animator.SetBool("isAttacking",false);
+            ChasePlayer();
+        }
+    }
+
+    private void OnTriggerStay(Collider other){
+        if(other.gameObject.tag=="Player"){
+            AttackPlayer();
+        }
+    }
 
 
 }
