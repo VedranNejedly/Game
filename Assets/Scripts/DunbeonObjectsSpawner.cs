78ac47c98aa35fab72a02278fffd3944c782b138
Vedran Nejedly <62423202+VedranNejedly@users.noreply.github.com>
2023-06-01 17:10:22 +0200

    Door animator for each door and fixed logic to door opening and closing.

--- Assets/Scripts/DunbeonObjectsSpawner.cs
+++ Assets/Scripts/DunbeonObjectsSpawner.cs
@@ -20,7 +20,12 @@ public class DunbeonObjectsSpawner : MonoBehaviour
  
         for(int i = 0;i<spawnPoints.Length;i++){
             rand=Random.Range(0,dunObjList.dungeonObjects.Length);
-            Instantiate(dunObjList.dungeonObjects[rand],spawnPoints[i].transform.position, spawnPoints[i].transform.rotation);
+            if(dunObjList.dungeonObjects[rand].name=="Trap Explosion"){
+                Instantiate(dunObjList.dungeonObjects[rand],new Vector3(spawnPoints[i].transform.position.x,-2.2f,spawnPoints[i].transform.position.z), spawnPoints[i].transform.rotation);
+            }
+            else{
+                Instantiate(dunObjList.dungeonObjects[rand],spawnPoints[i].transform.position, spawnPoints[i].transform.rotation);
+            }
 
 
         }
